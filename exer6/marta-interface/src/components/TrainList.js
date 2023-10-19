ba5860506cc9bb5df8570b0ec6ba4b361d79425c
Sahithya <sahithya.pasagada@gmail.com>
2023-10-18 22:29:55 -0400

    add load rendering

--- exer6/marta-interface/src/components/TrainList.js
+++ exer6/marta-interface/src/components/TrainList.js
@@ -1,19 +1,7 @@
 import Train from "./Train";
-import { useState } from "react";
-import { useEffect } from "react";
 
 export default function TrainList(props) {
-  // const {userColor, trainData} = props;
-  const {userColor} = props;
-  const [data, setData] = useState(null);
-
-  const API_URL = `http://13.59.196.129:3001/arrivals/${userColor.toLowerCase()}`
-
-  useEffect(() => {
-    fetch(API_URL)
-    .then(response => response.json())
-    .then(data => setData(data))
-  },[userColor])
+  const {userColor, data} = props;
 
   function direction() {
     if (userColor === "GOLD" | userColor === "RED") {
--- exer6/marta-interface/src/pages/LinesPage.js
+++ exer6/marta-interface/src/pages/LinesPage.js
@@ -1,47 +1,95 @@
-// get static data
-import stationData from '../server/stationData';
-import TrainList from '../components/TrainList';
-import NavBar from '../components/NavBar';
+import stationData from "../server/stationData";
+import TrainList from "../components/TrainList";
+import NavBar from "../components/NavBar";
+import React from "react";
+import ReactLoading from "react-loading";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function LinesPage() {
-  // initialize some currColor state
-  const[currColor, setCurrColor] = useState("GOLD");
+  const [currColor, setCurrColor] = useState("GOLD");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const API_URL = `http://13.59.196.129:3001/arrivals/${currColor.toLowerCase()}`;
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .then(() => setLoading(false));
+  }, [currColor]);
+
+  function loadingScreen() {
+    return (
+      <div className="loadingScreen">
+          <h2>Loading...</h2>
+          <div className="loading">
+          <ReactLoading
+              type="spin"
+              color="#F7CE2C"
+              width={400}
+          />
+          </div>
+      </div>
+  );
+  }
 
   return (
-    <div className='linesPages'>
-      <div className='colorButtonContainer'>
-            <button style={{ borderColor: '#E8B70C', backgroundColor: '#E8B70C',}} className='colorButton'
-              onClick= {() => {
+    <>
+      {loading ? (
+        <div>{loadingScreen()}</div>
+      ) : (
+        <div className="linesPages">
+          <div className="colorButtonContainer">
+            <button
+              style={{ borderColor: "#E8B70C", backgroundColor: "#E8B70C" }}
+              className="colorButton"
+              onClick={() => {
                 setCurrColor("GOLD");
-              }} 
-            >Gold</button>
+              }}
+            >
+              Gold
+            </button>
 
-            <button style={{ borderColor: '#ff0000', backgroundColor: '#ff0000',}} className='colorButton'
-              onClick= {() => {
+            <button
+              style={{ borderColor: "#ff0000", backgroundColor: "#ff0000" }}
+              className="colorButton"
+              onClick={() => {
                 setCurrColor("RED");
-              }} 
-            >Red</button>
+              }}
+            >
+              Red
+            </button>
 
-            <button style={{ borderColor: '#0000FF', backgroundColor: '#0000FF',}} className='colorButton'
-              onClick= {() => {
+            <button
+              style={{ borderColor: "#0000FF", backgroundColor: "#0000FF" }}
+              className="colorButton"
+              onClick={() => {
                 setCurrColor("BLUE");
-              }} 
-            >Blue</button>
+              }}
+            >
+              Blue
+            </button>
 
-            <button style={{ borderColor: '#008000', backgroundColor: '#008000',}} className='colorButton'
-              onClick= {() => {
+            <button
+              style={{ borderColor: "#008000", backgroundColor: "#008000" }}
+              className="colorButton"
+              onClick={() => {
                 setCurrColor("GREEN");
-              }} 
-            >Green</button>
+              }}
+            >
+              Green
+            </button>
+          </div>
 
-      </div>
-      <h1 className='heading'>{currColor}</h1>
-      <div className='container'>
-        <NavBar currColor={currColor} stationData={stationData} />
-        {/* <TrainList userColor={currColor} trainData={trainData} /> */}
-        <TrainList userColor={currColor}/>
-      </div>
-    </div>
-  )
+          <h1 className="heading">{currColor}</h1>
+          <div className="container">
+            <NavBar currColor={currColor} stationData={stationData} />
+            <TrainList userColor={currColor} data={data} />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
